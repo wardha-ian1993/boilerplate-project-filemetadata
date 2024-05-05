f462cc43f9f8e655e7eb5d6f10e5956d919dfdce
wardha-ian1993 <favianwardhana1993@gmail.com>
2024-05-05 20:48:47 +0700

    finalizing

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
+var bodyParser = require('body-parser');
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const multer = require('multer');
 
+const upload = multer({ dest: 'uploads/' });
 var app = express();
 
 app.use(cors());
@@ -11,7 +15,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
 
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
